69e288c2088b8d8ec3e00eb4bb30c04cd8f591e6
VionieNovencia <16520188@std.stei.itb.ac.id>
2022-03-13 11:15:05 +0700

    added item v1

---
+++ Item.cpp
@@ -0,0 +1,90 @@
+#include <Item.h>
+#include <iostream>
+
+Item :: Item(){
+    this-> ID = count++;
+    this-> name = "noname";
+    this-> type = "notype";
+    this-> quantity = 0;
+    this-> count++;
+}
+
+Item :: Item( string name, string type, int quantity){
+    this-> ID = count++;
+    this-> name = name;
+    this-> type = type;
+    this-> quantity = quantity;
+    this-> count++;
+}
+
+void Item ::  set_name(string name){
+    this-> name =  name;
+}
+
+string Item ::  get_name() const{
+    return this-> name;
+}
+
+void Item :: set_type(string type){
+    this-> type = type;
+}
+
+string Item :: get_type() const{
+    return this-> type;
+}
+
+void  Item :: set_quantity(int quantity){
+    this-> quantity =  quantity;
+}
+
+int Item :: get_quantity() const{
+    return this-> quantity;
+}
+
+void Item :: displayInfo() const{
+    std :: cout << "ID: " << this-> ID << std :: endl;
+    std :: cout << "Name: " << this-> name << std :: endl;
+    std :: cout << "Type: " << this-> type << std :: endl;
+    std :: cout << "Quantity: " << this-> quantity << std :: endl;
+}
+
+Tool :: Tool(): Item(){
+    this-> durability = 0;
+}
+
+Tool :: Tool( std :: string name, int quantity, int durability) : Item( name, "tools",quantity){
+    this-> durability = durability;
+}
+
+void Tool :: set_durability(int durability){
+    this-> durability = durability;
+}
+int Tool :: get_durability() const{
+    return this-> durability;
+}
+
+void Tool :: displayInfo() const{
+    Item :: displayInfo();
+    std :: cout << "Durability: " << this-> durability << std :: endl;
+}
+
+NonTool :: NonTool() : Item (){
+    this-> variant = "novariant";
+}
+
+NonTool :: NonTool(std :: string name, int quantity, std :: string variant) : Item(name,"nontools",quantity){
+    this-> variant = variant;
+}
+
+void NonTool :: set_variant(std :: string variant){
+    this-> variant = variant;
+}
+
+string NonTool :: get_variant() const{
+    return this-> variant;
+}
+
+void NonTool :: displayInfo() const{
+    Item :: displayInfo();
+    std :: cout << "Variant: " << this-> variant << std :: endl;
+}
---
+++ Item.h
@@ -0,0 +1,46 @@
+#ifndef ITEM_H
+#define ITEM_H
+#include <string>
+using namespace std;
+
+class Item {
+    private:
+        int ID;
+        std :: string name;
+        std :: string type;
+        int quantity;
+        int count;
+    public:
+        Item();
+        Item(std :: string name, std :: string type, int quantity);
+        void set_name(std :: string name);
+        std :: string get_name() const;
+        void set_type(std :: string type);
+        std :: string get_type() const;
+        void set_quantity(int quantity);
+        int get_quantity() const;
+        virtual void displayInfo() const;
+};
+
+class Tool : public Item{
+    private: 
+        int durability;
+    public:
+        Tool();
+        Tool( std :: string name, int quantity, int durability);
+        void set_durability(int durability);
+        int get_durability() const;
+        void displayInfo() const;
+};
+
+class NonTool : public Item{
+    private:
+        std :: string variant;
+    public:
+        NonTool();
+        NonTool(std :: string name, int quantity, std :: string variant);
+        void set_variant(std :: string variant);
+        std :: string get_variant() const;
+        void displayInfo() const;
+};
+#endif
