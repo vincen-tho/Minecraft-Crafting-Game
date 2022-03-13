465c71e3222fe285eae702bcc6ea8a694b016cb4
Muhammad Alif Putra Yasa <malifputrayasa@gmail.com>
2022-03-13 13:00:16 +0700

    Header Crafting

---
+++ Crafting.hpp
@@ -0,0 +1,112 @@
+#ifndef __CRAFTING_H__
+#define __CRAFTING_H__
+
+#include "Item.h"
+#include <string>
+
+#define RECIPE_MAX_SIZE 30
+
+class Recipe; // Forward declaration
+
+class CraftState {
+// Kelas ini merupakan array 3 x 3 di Crafting
+private:
+  string **slot; // Array 3 x 3
+  int *top_left;
+  int *dimension;
+
+public:
+  CraftState();
+  ~CraftState();
+
+  // Tambah Item
+  void addItem(Item i, int lokasi);
+
+  // Kembalikan Item
+  void returnItem(int lokasi);
+
+  // show
+  void show();
+
+  // Dobel supaya komutatif
+  friend bool operator==(const CraftState& cs, const Recipe& r);
+  friend bool operator==(const Recipe& r, const CraftState& cs);
+};
+
+class Recipe {
+// Resep
+private:
+  const int *dimension; // row x col
+  const string **input;
+  const string output; // Nama item output
+  const int output_q;  // Jumlah item output
+public:
+  // Bikin Resep
+  //
+  // Input :
+  //  dimension : int[2]
+  //  input     : string[dimension[0]][dimension[1]]
+  //  output    : string
+  //  output_q  : int
+  Recipe(int *dimension, string **input, string output, int output_q);
+  ~Recipe();
+
+  // Dobel supaya komutatif
+  friend bool operator==(const CraftState& cs, const Recipe& r);
+  friend bool operator==(const Recipe& r, const CraftState& cs);
+};
+
+class AllRecipe {
+// List Resep
+private:
+  Recipe recipes[RECIPE_MAX_SIZE];
+  int size;
+
+public:
+  AllRecipe();
+
+  // Menambahkan Recipe
+  void addRecipe(int *dimension, string **input, string output, int output_q);
+
+  // Mencari item di recipes
+  //
+  // Input : Crafstate
+  // Output: Lokasi resep di recipes, jika gak ada return -1
+  int search_item(CraftState& cs) const;
+};
+
+class Crafting {
+// Main class untuk fitur Crafting
+private:
+  CraftState* cs; // Array 3 x 3
+  AllRecipe ar; // Kumpulan Resep
+  string output; // Nama item yang akan dihasilkan
+public:
+  Crafting();
+
+  // Mendisplay CraftState dan Output
+  void show() const;
+
+  // Menambahkan item ke lokasi
+  // row = lokasi `div` 3
+  // col = lokasi `mod` 3
+  void add_item(Item i, int lokasi);
+  
+  // Mengembalikan item di lokasi
+  // row = lokasi `div` 3
+  // col = lokasi `mod` 3
+  Item return_item(int lokasi);
+
+  // Jika bisa ngecraft
+  //    return Item
+  //    kosongin CraftState
+  //    kosongin Output
+  // Else
+  //    Do Nothing
+  Item craft();
+
+  // Mengosongkan CraftState dan Output
+  void refreshCraftState();
+};
+
+#endif
