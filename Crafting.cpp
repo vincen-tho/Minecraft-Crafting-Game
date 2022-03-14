a32177501752794706b021836f6904e185544bf8
Muhammad Alif Putra Yasa <malifputrayasa@gmail.com>
2022-03-14 14:39:06 +0700

    Sebagian Crafting.cpp

---
+++ Crafting.cpp
@@ -0,0 +1,129 @@
+#include "Crafting.hpp"
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+CraftState::CraftState() {
+  this->slot = new string *[3];
+  for (int i = 0; i < 3; i++) {
+    this->slot[i] = new string[3];
+    for (int j = 0; j < 3; j++) {
+      this->slot[i][j] = "-";
+    }
+  }
+
+  this->dimension = new int[2];
+  this->top_left = new int[2];
+}
+
+CraftState::~CraftState() {
+  for (int i = 0; i < 3; i++) {
+    delete[] this->slot[i];
+  }
+  delete[] this->slot;
+  delete[] this->dimension;
+  delete[] this->top_left;
+}
+
+void CraftState::addItem(Item i, int lokasi) {
+  int row = lokasi / 3;
+  int col = lokasi % 3;
+
+  this->slot[row][col] = i.get_name();
+}
+
+void CraftState::returnItem(int lokasi) {
+  // Belum diimplementasikan
+  // Perlu cara untuk ngidentifikasi tipe dari item
+  // (string -> Item)
+}
+
+void CraftState::show() {
+  for (int i = 0; i < 3; i++) {
+    for (int j; j < 3; j++) {
+      cout << "[ " << this->slot << " ]"
+           << " ";
+    }
+    cout << endl;
+  }
+}
+
+bool operator==(const CraftState &cs, const Recipe &r) {
+  if (cs.dimension[0] != r.dimension[0] || cs.dimension[1] != r.dimension[1]) {
+    return false;
+  } else { // Dimensi-nya sama
+    for (int i = 0; i < r.dimension[0]; i++) {
+      for (int j = 0; j < r.dimension[1]; j++) {
+        if (cs.slot[cs.top_left[0] + i][cs.top_left[1] + j] != r.input[i][j]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+bool operator==(const Recipe &r, const CraftState &cs) { return cs == r; }
+
+Recipe::Recipe() {
+  this->dimension = NULL;
+  this->input = NULL;
+  this->output = "-";
+  this->output_q = 0;
+}
+
+Recipe::Recipe(int *dimension, string **input, string output, int output_q) {
+  this->dimension = new int[2];
+  this->dimension[0] = dimension[0];
+  this->dimension[1] = dimension[1];
+  this->input = new string *[this->dimension[0]];
+  for (int i = 0; i < this->dimension[0]; i++) {
+    this->input[i] = new string[this->dimension[1]];
+    for (int j = 0; j < this->dimension[1]; j++) {
+      this->input[i][j] = input[i][j];
+    }
+  }
+  this->output = output;
+  this->output_q = output_q;
+}
+
+Recipe::~Recipe() {
+  for (int i = 0; i < this->dimension[1]; i++) {
+    delete[] this->input[i];
+  }
+  delete[] this->input;
+  delete[] this->dimension;
+}
+
+void Recipe::operator=(const Recipe &r) {
+  for (int i = 0; i < this->dimension[0]; i++) {
+      delete [] this->input[i];
+  }
+  delete [] this->input;
+  this->dimension[0] = r.dimension[0];
+  this->dimension[1] = r.dimension[1];
+  this->input = new string*[this->dimension[0]];
+  for (int i = 0; i < this->dimension[0]; i++) {
+    this->input[i] = new string[this->dimension[1]];
+    for (int j = 0; j < this->dimension[1]; j++) {
+        this->input[i][j] = r.input[i][j];
+    }
+  }
+  this->output = r.output;
+  this->output_q = r.output_q;
+}
+
+AllRecipe::AllRecipe() {
+  this->recipes = std::vector<Recipe>();
+}
+
+void AllRecipe::addRecipe(int *dimension, string **input, string output,
+                          int output_q) {
+    this->recipes.push_back(Recipe(dimension, input, output, output_q));
+}
+
+int AllRecipe::search_item(CraftState &cs) const{
+    return find(this->recipes.begin(), this->recipes.end(), cs) - this->recipes.begin();
+}
--- Crafting.hpp
+++ Crafting.hpp
@@ -3,13 +3,16 @@
 
 #include "Item.h"
 #include <string>
+#include <vector>
+
+using namespace std;
 
 #define RECIPE_MAX_SIZE 30
 
 class Recipe; // Forward declaration
 
 class CraftState {
-// Kelas ini merupakan array 3 x 3 di Crafting
+  // Kelas ini merupakan array 3 x 3 di Crafting
 private:
   string **slot; // Array 3 x 3
   int *top_left;
@@ -29,17 +32,17 @@ public:
   void show();
 
   // Dobel supaya komutatif
-  friend bool operator==(const CraftState& cs, const Recipe& r);
-  friend bool operator==(const Recipe& r, const CraftState& cs);
+  friend bool operator==(const CraftState &cs, const Recipe &r);
+  friend bool operator==(const Recipe &r, const CraftState &cs);
 };
 
 class Recipe {
-// Resep
+  // Resep
 private:
-  const int *dimension; // row x col
-  const string **input;
-  const string output; // Nama item output
-  const int output_q;  // Jumlah item output
+  int *dimension; // row x col
+  string **input;
+  string output; // Nama item output
+  int output_q;  // Jumlah item output
 public:
   // Bikin Resep
   //
@@ -48,39 +51,43 @@ public:
   //  input     : string[dimension[0]][dimension[1]]
   //  output    : string
   //  output_q  : int
-  Recipe(int *dimension, string **input, string output, int output_q);
+  Recipe();
+  Recipe(int *dimension,  string **input,  string output,
+          int output_q);
+  Recipe(const Recipe& r);
   ~Recipe();
 
+  void operator=(const Recipe &r);
   // Dobel supaya komutatif
-  friend bool operator==(const CraftState& cs, const Recipe& r);
-  friend bool operator==(const Recipe& r, const CraftState& cs);
+  friend bool operator==(const CraftState &cs, const Recipe &r);
+  friend bool operator==(const Recipe &r, const CraftState &cs);
 };
 
 class AllRecipe {
-// List Resep
+  // List Resep
 private:
-  Recipe recipes[RECIPE_MAX_SIZE];
-  int size;
+  vector<Recipe> recipes;
 
 public:
   AllRecipe();
 
   // Menambahkan Recipe
-  void addRecipe(int *dimension, string **input, string output, int output_q);
+  void addRecipe(int *dimension, string **input,
+                 string output, int output_q);
 
   // Mencari item di recipes
   //
   // Input : Crafstate
   // Output: Lokasi resep di recipes, jika gak ada return -1
-  int search_item(CraftState& cs) const;
+  int search_item(CraftState &cs) const;
 };
 
 class Crafting {
-// Main class untuk fitur Crafting
+  // Main class untuk fitur Crafting
 private:
-  CraftState* cs; // Array 3 x 3
-  AllRecipe ar; // Kumpulan Resep
-  string output; // Nama item yang akan dihasilkan
+  CraftState *cs; // Array 3 x 3
+  AllRecipe ar;   // Kumpulan Resep
+  string output;  // Nama item yang akan dihasilkan
 public:
   Crafting();
 
@@ -91,7 +98,12 @@ public:
   // row = lokasi `div` 3
   // col = lokasi `mod` 3
   void add_item(Item i, int lokasi);
-  
+
+  // Menambahkan resep ke ar
+  // dimension dan input akan di hard copy
+  void addRecipe(int *dimension, string **input,
+                 string output, int output_q);
+
   // Mengembalikan item di lokasi
   // row = lokasi `div` 3
   // col = lokasi `mod` 3
