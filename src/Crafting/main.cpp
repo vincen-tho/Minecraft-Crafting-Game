323c8a020b5382484ed13c617325529f7fd9f3c8
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-03-17 22:21:01 +0700

    command MOVE craft ke inventory prototype

--- src/Crafting/main.cpp
+++ src/Crafting/main.cpp
@@ -1,10 +1,12 @@
 #include "../lib.hpp"
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 int main() {
   Crafting Craft;
+  Inventory Inv;
   load_recipes(Craft);
   load_items(Craft);
   Tool T1("Tool 1", 4);
@@ -13,5 +15,36 @@ int main() {
   Craft.add_item(T1, 0);
   Craft.add_item(T2, 1);
   Craft.show();
+  
+  string command;
+  cin >> command;
+  if (command == "MOVE") {
+
+    string slotSrc, slotDest;
+    int idSlotSrc, idSlotDest;
+    char typeSlotSrc, typeSlotDst;
+    int slotQty;
+    // need to handle multiple destinations
+    cin >> slotSrc >> slotQty >> slotDest;
+    // cout << "TODO" << endl;
+    istringstream sSrc(slotSrc);
+    istringstream sDst(slotDest);
+        
+    sSrc >> typeSlotSrc >> idSlotSrc;
+    sDst >> typeSlotDst >> idSlotDest;
+
+    if(typeSlotSrc == 'C' && typeSlotDst == 'I' && slotQty == 1){
+          
+      // kembaliin item yang ada di slot craft
+      // Item t = Craft.return_item(idSlotSrc);
+      // Inv.add_item(t, slotQty);
+    }
+  }
+    
+  
+
+  Craft.show();
+  // showInventory(Inv);
+
   return 0;
 }
