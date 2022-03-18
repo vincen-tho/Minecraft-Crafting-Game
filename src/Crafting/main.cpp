8abdddd19b40bba80c0d416f14c0408dff351788
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-03-18 08:36:13 +0700

    Command move with exception

--- src/Crafting/main.cpp
+++ src/Crafting/main.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+void showInventory(Inventory Inv){
+
+  for(int i = 0; i < 27; i++){
+    cout << Inv[i].first.get_name() << " : " << Inv[i].second << endl; 
+  }
+
+}
+
+
 int main() {
   Crafting Craft;
   Inventory Inv;
@@ -26,25 +35,48 @@ int main() {
     int slotQty;
     // need to handle multiple destinations
     cin >> slotSrc >> slotQty >> slotDest;
-    // cout << "TODO" << endl;
+    
     istringstream sSrc(slotSrc);
     istringstream sDst(slotDest);
         
     sSrc >> typeSlotSrc >> idSlotSrc;
     sDst >> typeSlotDst >> idSlotDest;
 
+    // CRAFT -> INVENTORY
     if(typeSlotSrc == 'C' && typeSlotDst == 'I' && slotQty == 1){
           
       // kembaliin item yang ada di slot craft
-      // Item t = Craft.return_item(idSlotSrc);
-      // Inv.add_item(t, slotQty);
-    }
-  }
-    
-  
+      // buat ngecek di lokasi itu ada itemnya atau engga
+      // harusnya udah di handle di method return_item nya
+      try{
 
+        if(idSlotDest > 26){
+          BaseException *E = new InvalidNumberException(idSlotDest);
+          throw(E);
+        }
+        
+        if(idSlotSrc > 8){
+          BaseException *E = new InvalidNumberException(idSlotSrc);
+          throw (E);
+        }
+
+        Item* temp = Craft.return_item(idSlotSrc);
+        // method Inv.add_item nya perlu dimodifikasi jadi berdasarkan item, quantity, dan lokasi
+        Inv.add_item(*temp, slotQty);
+
+      }catch(BaseException *E){
+        E->printMessage();
+      }
+         
+    } else if (typeSlotSrc == 'I' && typeSlotDst == 'I' && slotQty == 1){
+
+      // ISI BUAT YANG NUMPUK ITEM DI INVENTORY
+
+    } 
+  }
+  // make drive_test TDIR=Crafting
   Craft.show();
-  // showInventory(Inv);
+  showInventory(Inv);
 
   return 0;
 }
